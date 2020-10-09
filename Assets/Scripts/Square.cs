0553ffd65ed9989bef6fe4e976422ccc8e81c98a
alessandro <anf9790@g.rit.edu>
2020-10-08 21:33:52 -0400

    BIG COMMIT
    I forgot commits are a thing! I've been working on the grid, the size seems to be an issue (I kinda forgot how Unity works with screen sizes; we definately need to scale up our pixel art...).
    I have a grid of spot objects, now I need to create objects to sit on thos spots, and I need to get the controls working.

---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    #region Fields
+    // The position of the top left corner of the square.
+    public Vector2 position;
+    // The size of the square in pixels.
+    private float size;
+    // Whether or not the square has an object placed on it.
+    private bool empty;
+    #endregion
+
+    #region Properties
+    public Vector2 Position
+    {
+        get { return position; }
+        set { position = value; }
+    }
+    public float Size
+    {
+        get { return size; }
+        set { size = value; }
+    }
+    public bool isEmpty
+    {
+        get { return empty; }
+        set { empty = value; }
+    }
+    // Getting the points on the square.
+    public Vector2 getTopLeft
+    {
+        get { return position; }
+    }
+    public Vector2 getTopRight
+    {
+        get { return new Vector2(position.x + size, position.y); }
+    }
+    public Vector2 getBottomRight
+    {
+        get { return new Vector2(position.x + size, position.y + size); }
+    }
+    public Vector2 getBottomLeft
+    {
+        get { return new Vector2(position.x, position.y + size); }
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = 1.0f;
+        empty = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = position;
+    }
+}
